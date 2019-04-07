e63312b60c3deb1561b40077ad2bd2bc0e9fc31b
Greg Kline <gkline@stetson.edu>
2019-04-07 16:24:39 -0400

    Add files via upload

--- Connector.java
+++ Connector.java
@@ -1,10 +1,12 @@
-package tcpConnection;
-
 import java.util.*;
 
 public class Connector {
 
 	public int totalPackets;
+
+	public static int ssthresh = 32;
+
+	public static boolean slowStart = true;
 
 	public static void main(String[] args) {
 
@@ -19,17 +21,16 @@ public class Connector {
 		int mss = sc.nextInt();
 		System.out.println("Enter probability of packet loss:");
 		double ploss = sc.nextDouble();
-		
-		
-		int packs =50;
+
+		int packs = 50;
 		System.out.println("# of packets: " + packs);
-		Packet[] packets = new Packet[packs+2];
+		Packet[] packets = new Packet[packs + 2];
 		for (int i = 0; i < packets.length; i++) {
 			packets[i] = new Packet();
-			if(data - mss > 0) {
-			packets[i].setData(mss);
-			data -= mss;
-			}else {
+			if (data - mss > 0) {
+				packets[i].setData(mss);
+				data -= mss;
+			} else {
 				packets[i].setData(data);
 			}
 		}
@@ -37,28 +38,48 @@ public class Connector {
 		packets[3].setSeqNum(packets[2].getackNum());
 		packets[3].setAcknum((int) ((int) packets[2].getSeqNum() + packets[2].getData()));
 		
-		for (int i = 0; i < packets.length; i++) {
-			Random rand = new Random();
-			int low = 0;
-			int high = 100;
-			int result = rand.nextInt(high - low) + low;
+		if(slowStart) {
+			int i = 3;
 			
-			double lost = ploss * 100;
-			
-			System.out.println(
-					"PACKET # " + i +" : ESTABLISHING CONNECTION: " + "SEQ: " + packets[i].getSeqNum() + " ACK:" + packets[i].getackNum() + " DATA: " + packets[i].getData());
-			if(i > 2) {
-				transferData(packets, i, result, lost, dupAckCounter);
+			while(i < ssthresh && slowStart == true) {
+				Random rand = new Random();
+				int low = 0;
+				int high = 100;
+				int tempi = i * 2;
+				int result = rand.nextInt(high - low) + low;
+				double lost = ploss * 100;
+				for(int k = i;k<=tempi;k++) {
+					transferData(packets,i,result,lost,dupAckCounter);
+				}
+				i = tempi;
 			}
+			System.out.print("cwnd: " + i);
+		}
+		
+		
+		
+//		if (i > 2 && slowStart == true) {
+//			while(j < ssthresh) {
+//			int tempj = j*2;
+//			
+//			transferData(packets, i, result, lost, dupAckCounter);
+//			}
+//		}
+		
+		
+		for (int i = 0; i < packets.length; i++) {
+			System.out.println("PACKET # " + i + " : ESTABLISHING CONNECTION: " + "SEQ: " + packets[i].getSeqNum()
+					+ " ACK:" + packets[i].getackNum() + " DATA: " + packets[i].getData());
+			
 		}
 
 	}
 
 	public static void threeWayHS(Packet packet[]) {
 		Random ran = new Random();
-		
+
 		Packet sPacket = new Packet();
-		
+
 		packet[0].setSeqNum(ran.nextInt(99999));
 		// syn-ack; these are server packets, so may need to separate from array of
 		// 'total packets'
@@ -67,55 +88,54 @@ public class Connector {
 		// ack
 		packet[2].setAcknum(packet[1].getSeqNum() + 1);
 		packet[2].setSeqNum(packet[1].getackNum());
-		System.out.println("Syn Ack: " + "SEQ: " + sPacket.getSeqNum() + " ACK: " + sPacket.getackNum() );
+		System.out.println("Syn Ack: " + "SEQ: " + sPacket.getSeqNum() + " ACK: " + sPacket.getackNum());
 	}
 
 	/**
 	 * 
-	 * @param packet
-	 *            array of all packets
-	 * @param i
-	 *            index of packet from array
+	 * @param packet array of all packets
+	 * @param i      index of packet from array
 	 */
 	public static void transferData(Packet packet[], int i, int random, double lost, int dupAckCounter) {
 		// server packet, probably will have to update away from 'total packets'
 		if (packet[i + 1] != null) {
-			
-			while(lost <= random) {
+
+			while (lost <= random) {
 				System.out.println("Packet Lost!");
 				dupAckCounter++;
 				Random rand = new Random();
 				int low = 0;
 				int high = 100;
 				random = rand.nextInt(high - low) + low;
-				
-				if(dupAckCounter == 3) {
+
+				if (dupAckCounter == 3) {
 					System.out.println("Congest Avoidance Starts Here --->");
+					slowStart = false;
 				}
-				//random = rand.nextInt(high - low) + low
+				// random = rand.nextInt(high - low) + low
 			}
-						
+
 			// client variables
 			int cSeq = packet[i].getSeqNum();
 			int cAck = packet[i].getackNum();
 			float cData = packet[i].getData();
-			
+
 			// set 'server packet' ack and seq
-			packet[i+1].setSeqNum(cAck);
-			packet[i+1].setAcknum((int) (cSeq + cData));
-			
+			packet[i + 1].setSeqNum(cAck);
+			packet[i + 1].setAcknum((int) (cSeq + cData));
+
 			// future client packet variables
-			int cSeq2 = packet[i+2].getSeqNum();
-			int cAck2 = packet[i+2].getackNum();
-			
+			int cSeq2 = packet[i + 2].getSeqNum();
+			int cAck2 = packet[i + 2].getackNum();
+
 			// update what next client packet should look like
-			packet[i+2].setSeqNum(packet[i+1].getackNum());
-			packet[i+2].setAcknum(packet[i+1].getSeqNum());
-			
+			packet[i + 2].setSeqNum(packet[i + 1].getackNum());
+			packet[i + 2].setAcknum(packet[i + 1].getSeqNum());
+
 //			if(lost <= random) {
 //				System.out.println("Packet Lost!");
 //			}
-			
+
 		} else {
 			System.out.println("no more packets!");
 			System.exit(0);
---
+++ Packet.java
@@ -0,0 +1,29 @@
+public class Packet {
+	private int seqNum=0;
+	private int acknum=0;
+	private float data=0;
+	
+	public int getSeqNum(){
+		return seqNum;
+	}
+	
+	public int getackNum(){
+		return acknum;
+	}
+	
+	public float getData(){
+		return data;
+	}
+	
+	public void setSeqNum(int num) {
+		seqNum = num;
+	}
+	
+	public void setAcknum(int f) {
+		acknum = f;
+	}
+	
+	public void setData(float num) {
+		data = num;
+	}
+}
