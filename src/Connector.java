0babd3efc8859b5a4c3ae911f24bdfe00e902ec1
Greg Kline <gkline@stetson.edu>
2019-04-07 13:58:47 -0400

    Add files via upload

--- src/Connector.java
+++ src/Connector.java
@@ -15,23 +15,36 @@ public class Connector {
 		System.out.println("Enter MSS (bytes)");
 		int mss = sc.nextInt();
 		
-		int packs = data/mss;
-		System.out.println("# of packets" + packs);
-		Packet[] packets = new Packet[packs];
+		int packs =50;
+		System.out.println("# of packets: " + packs);
+		Packet[] packets = new Packet[packs+2];
 		for (int i = 0; i < packets.length; i++) {
 			packets[i] = new Packet();
+			if(data - mss > 0) {
+			packets[i].setData(mss);
+			data -= mss;
+			}else {
+				packets[i].setData(data);
+			}
 		}
 		threeWayHS(packets);
+		
 		for (int i = 0; i < packets.length; i++) {
-
+			
 			System.out.println(
-					"ESTABLISHING CONNECTION: " + "SEQ: " + packets[i].getSeqNum() + " ACK:" + packets[i].getackNum());
+					"PACKET # " + i +" : ESTABLISHING CONNECTION: " + "SEQ: " + packets[i].getSeqNum() + " ACK:" + packets[i].getackNum() + " DATA: " + packets[i].getData());
+			if(i > 2) {
+				transferData(packets, i-1);
+			}
 		}
 
 	}
 
 	public static void threeWayHS(Packet packet[]) {
 		Random ran = new Random();
+		
+		Packet sPacket = new Packet();
+		
 		packet[0].setSeqNum(ran.nextInt(99999));
 		// syn-ack; these are server packets, so may need to separate from array of
 		// 'total packets'
@@ -40,6 +53,7 @@ public class Connector {
 		// ack
 		packet[2].setAcknum(packet[1].getSeqNum() + 1);
 		packet[2].setSeqNum(packet[1].getackNum() + 1);
+		System.out.println("Syn Ack: " + "SEQ: " + sPacket.getSeqNum() + " ACK: " + sPacket.getackNum() );
 	}
 
 	/**
@@ -51,9 +65,29 @@ public class Connector {
 	 */
 	public static void transferData(Packet packet[], int i) {
 		// server packet, probably will have to update away from 'total packets'
-		if (packet[i + 1] != null) {
-			int temppackackNum = packet[i + 1].getackNum();
-			packet[i + 1].setAcknum((int) (temppackackNum + packet[i].getData()));
+		if (packet[i] != null) {
+			
+			// pseudo 'server' response packet
+			Packet sPacket = new Packet();
+			
+			// client variables
+			int cSeq = packet[i].getSeqNum();
+			int cAck = packet[i].getackNum();
+			float cData = packet[i].getData();
+			
+			// set 'server packet' ack and seq
+			packet[i+1].setSeqNum(cAck);
+			packet[i+1].setAcknum((int) (cSeq + cData));
+			
+			// future client packet variables
+			int cSeq2 = packet[i+2].getSeqNum();
+			int cAck2 = packet[i+2].getackNum();
+			
+			// update what next client packet should look like
+			packet[i+2].setSeqNum(packet[i+1].getackNum());
+			packet[i+2].setAcknum(packet[i+1].getSeqNum());
+
+			
 		} else {
 			System.out.println("no more packets!");
 			System.exit(0);
