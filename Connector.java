327bc43386ecca1f93677a4b881d56094c2c2349
Frederick Robert Wittmann <41751970+freddiewittmann@users.noreply.github.com>
2019-04-07 22:32:54 -0400

    Add files via upload

--- Connector.java
+++ Connector.java
@@ -1,3 +1,5 @@
+package tcpConnection;
+
 import java.util.*;
 
 public class Connector {
@@ -7,6 +9,9 @@ public class Connector {
 	public static int ssthresh = 32;
 
 	public static boolean slowStart = true;
+
+	public static ArrayList<Packet> sPackets = new ArrayList<Packet>();
+	public static ArrayList<Packet> cPackets = new ArrayList<Packet>();
 
 	public static void main(String[] args) {
 
@@ -20,127 +25,90 @@ public class Connector {
 		System.out.println("Enter MSS (bytes)");
 		int mss = sc.nextInt();
 		System.out.println("Enter probability of packet loss:");
-		double ploss = sc.nextDouble();
+		float ploss = sc.nextFloat();
 
-		int packs = 50;
-		System.out.println("# of packets: " + packs);
-		Packet[] packets = new Packet[packs + 2];
-		for (int i = 0; i < packets.length; i++) {
-			packets[i] = new Packet();
-			if (data - mss > 0) {
-				packets[i].setData(mss);
-				data -= mss;
-			} else {
-				packets[i].setData(data);
-			}
-		}
-		threeWayHS(packets);
-		packets[3].setSeqNum(packets[2].getackNum());
-		packets[3].setAcknum((int) ((int) packets[2].getSeqNum() + packets[2].getData()));
-		
-		if(slowStart) {
-			int i = 3;
-			
-			while(i < ssthresh && slowStart == true) {
+		threeWayHS();
+		toServer(cPackets.get(cPackets.size() - 1));
+		toClient();
+		toServer(cPackets.get(cPackets.size() - 1));
+		toClient();
+		toServer(cPackets.get(cPackets.size() - 1));
+		toClient();
+		try {
+			for (int i = 0; i < cPackets.size(); i++) {
 				Random rand = new Random();
 				int low = 0;
 				int high = 100;
-				int tempi = i * 2;
 				int result = rand.nextInt(high - low) + low;
-				double lost = ploss * 100;
-				for(int k = i;k<=tempi;k++) {
-					transferData(packets,i,result,lost,dupAckCounter);
+				
+				while(result <= ploss * 100) {
+					//System.out.println("Packet Lost");
+					System.out.println("CLIENT - SEQ: " + cPackets.get(i).getSeqNum() + " ACK " + cPackets.get(i).getackNum());
+					result = rand.nextInt(high - low) + low;
+					dupAckCounter++;
+					
+					if(dupAckCounter == 3) {
+						System.out.println("Begin Congestion Avoidance");
+						break;
+					}
 				}
-				i = tempi;
+				
+				if(dupAckCounter == 0 && result > ploss) {
+				System.out.println(
+						"CLIENT - SEQ: " + cPackets.get(i).getSeqNum() + " ACK " + cPackets.get(i).getackNum());
+				}
+				System.out.println(
+						"SERVER - SEQ: " + sPackets.get(i).getSeqNum() + " ACK " + sPackets.get(i).getackNum());
 			}
-			System.out.print("cwnd: " + i);
+		} catch (Exception e) {
+			System.out.println("No more packets!");
 		}
-		
-		
-		
-//		if (i > 2 && slowStart == true) {
-//			while(j < ssthresh) {
-//			int tempj = j*2;
-//			
-//			transferData(packets, i, result, lost, dupAckCounter);
-//			}
-//		}
-		
-		
-		for (int i = 0; i < packets.length; i++) {
-			System.out.println("PACKET # " + i + " : ESTABLISHING CONNECTION: " + "SEQ: " + packets[i].getSeqNum()
-					+ " ACK:" + packets[i].getackNum() + " DATA: " + packets[i].getData());
-			
-		}
-
 	}
 
-	public static void threeWayHS(Packet packet[]) {
+	public static void threeWayHS() {
 		Random ran = new Random();
-
 		Packet sPacket = new Packet();
-
-		packet[0].setSeqNum(ran.nextInt(99999));
+		Packet c1 = new Packet();
+		c1.setSeqNum(ran.nextInt(99999));
+		cPackets.add(c1);
+		Packet s1 = new Packet();
 		// syn-ack; these are server packets, so may need to separate from array of
 		// 'total packets'
-		packet[1].setAcknum(packet[0].getSeqNum() + 1);
-		packet[1].setSeqNum(0);
+		s1.setAcknum(cPackets.get(cPackets.size() - 1).getSeqNum() + 1);
+		s1.setSeqNum(0);
+		sPackets.add(s1);
 		// ack
-		packet[2].setAcknum(packet[1].getSeqNum() + 1);
-		packet[2].setSeqNum(packet[1].getackNum());
-		System.out.println("Syn Ack: " + "SEQ: " + sPacket.getSeqNum() + " ACK: " + sPacket.getackNum());
+		Packet c2 = new Packet();
+		c2.setAcknum(s1.getSeqNum() + 1);
+		c2.setSeqNum(s1.getackNum());
+		cPackets.add(c2);
 	}
 
 	/**
 	 * 
-	 * @param packet array of all packets
-	 * @param i      index of packet from array
+	 * @param packet
+	 *            array of all packets
+	 * @param i
+	 *            index of packet from array
 	 */
-	public static void transferData(Packet packet[], int i, int random, double lost, int dupAckCounter) {
-		// server packet, probably will have to update away from 'total packets'
-		if (packet[i + 1] != null) {
+	public static void toServer(Packet packet) {
+		
+		Packet sPacket = new Packet();
+		sPacket.setSeqNum(packet.getackNum());
+		sPacket.setAcknum((int) (packet.getSeqNum() + packet.getData()));
+		sPackets.add(sPacket);
+	}
 
-			while (lost <= random) {
-				System.out.println("Packet Lost!");
-				dupAckCounter++;
-				Random rand = new Random();
-				int low = 0;
-				int high = 100;
-				random = rand.nextInt(high - low) + low;
-
-				if (dupAckCounter == 3) {
-					System.out.println("Congest Avoidance Starts Here --->");
-					slowStart = false;
-				}
-				// random = rand.nextInt(high - low) + low
-			}
-
-			// client variables
-			int cSeq = packet[i].getSeqNum();
-			int cAck = packet[i].getackNum();
-			float cData = packet[i].getData();
-
-			// set 'server packet' ack and seq
-			packet[i + 1].setSeqNum(cAck);
-			packet[i + 1].setAcknum((int) (cSeq + cData));
-
-			// future client packet variables
-			int cSeq2 = packet[i + 2].getSeqNum();
-			int cAck2 = packet[i + 2].getackNum();
-
-			// update what next client packet should look like
-			packet[i + 2].setSeqNum(packet[i + 1].getackNum());
-			packet[i + 2].setAcknum(packet[i + 1].getSeqNum());
-
-//			if(lost <= random) {
-//				System.out.println("Packet Lost!");
-//			}
-
-		} else {
-			System.out.println("no more packets!");
-			System.exit(0);
-		}
-
+	/**
+	 * Send the latest packet from the server back to the client
+	 */
+	public static void toClient() {
+		Packet sPacket = sPackets.get(sPackets.size() - 1);
+		Packet cPacket = new Packet();
+		cPacket.setAcknum(sPacket.getSeqNum());
+		cPacket.setSeqNum(sPacket.getackNum());
+		cPacket.setData(10);
+		cPackets.add(cPacket);
 	}
 
 }
