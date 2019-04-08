2bb550efac8e8cfa5a56a945b14135c942cd4fa1
Greg Kline <gkline@stetson.edu>
2019-04-08 08:41:57 -0400

    Add files via upload

--- src/Connector.java
+++ src/Connector.java
@@ -4,12 +4,19 @@ public class Connector {
 
 	public int totalPackets;
 
-	public static int ssthresh = 32;
-
+	public static float ssthresh = 32;
+	public static float cwnd;
+	public static int nLost = 0;
 	public static boolean slowStart = true;
 
 	public static ArrayList<Packet> sPackets = new ArrayList<Packet>();
 	public static ArrayList<Packet> cPackets = new ArrayList<Packet>();
+	public static ArrayList<Float> times = new ArrayList<Float>();
+	public static float pubData;
+	public static float pubMSS;
+	public static float pubProb;
+	public static float pubTime;
+	public static float totalLost = 0;
 
 	public static void main(String[] args) {
 
@@ -18,20 +25,29 @@ public class Connector {
 
 		System.out.println("Enter amount of data (bytes)");
 		int data = sc.nextInt();
-		System.out.println("Enter latency (ms)");
+		pubData = data;
+		System.out.println("Enter average time between messages (ms)");
 		int time = sc.nextInt();
+		pubTime = time;
 		System.out.println("Enter MSS (bytes)");
 		int mss = sc.nextInt();
+		pubMSS = mss;
 		System.out.println("Enter probability of packet loss:");
 		float ploss = sc.nextFloat();
+		pubProb = ploss;
 
+		// initiate connection
 		threeWayHS();
-		toServer(cPackets.get(cPackets.size() - 1));
-		toClient();
-		toServer(cPackets.get(cPackets.size() - 1));
-		toClient();
-		toServer(cPackets.get(cPackets.size() - 1));
-		toClient();
+
+		// start slow start
+		sStart();
+//		congestionAvoidance();
+
+		// the final packet
+		toServer(cPackets.get(cPackets.size() - 1), pubProb);
+		toClient(pubData);
+
+		System.out.println("DATA LEFT " + pubData);
 		try {
 			for (int i = 0; i < cPackets.size(); i++) {
 				System.out.println(
@@ -41,6 +57,7 @@ public class Connector {
 			}
 		} catch (Exception e) {
 			System.out.println("No more packets!");
+			System.out.println("CWND: " + cwnd);
 		}
 	}
 
@@ -66,27 +83,106 @@ public class Connector {
 	/**
 	 * 
 	 * @param packet
-	 *            array of all packets
-	 * @param i
-	 *            index of packet from array
+	 *            Packet you would like to send to server
 	 */
-	public static void toServer(Packet packet) {
-		Packet sPacket = new Packet();
-		sPacket.setSeqNum(packet.getackNum());
-		sPacket.setAcknum((int) (packet.getSeqNum() + packet.getData()));
-		sPackets.add(sPacket);
+	public static void toServer(Packet packet, float pSuccess) {
+		System.out.println("num lost: " + nLost);
+		// send successful packet back if not lost and reset lost counter if works
+		if (isLost(pSuccess) == false && nLost < 3) {
+			Packet sPacket = new Packet();
+			sPacket.setSeqNum(packet.getackNum());
+			sPacket.setAcknum((int) (packet.getSeqNum() + packet.getData()));
+			sPackets.add(sPacket);
+			nLost = 0;
+			System.out.println("RESET");
+			// server will send back dupe acks 3 times
+		} else if (nLost <= 3 && nLost > 0) {
+			System.out.println("dupe ack");
+			sPackets.add(sPackets.get(sPackets.size() - 1));
+
+		}
 	}
 
 	/**
 	 * Send the latest packet from the server back to the client
 	 */
-	public static void toClient() {
+	public static void toClient(float pubData2) {
+
 		Packet sPacket = sPackets.get(sPackets.size() - 1);
 		Packet cPacket = new Packet();
+		cPacket.setData(pubData2);
 		cPacket.setAcknum(sPacket.getSeqNum());
 		cPacket.setSeqNum(sPacket.getackNum());
-		cPacket.setData(10);
 		cPackets.add(cPacket);
+
+	}
+
+	/**
+	 * Decide if a packet will be lost
+	 * 
+	 * @param prob
+	 *            chance packet will be lost
+	 * @return if true, packet is lost; else packet isn't lost
+	 */
+	public static boolean isLost(float prob) {
+		Random ran = new Random();
+		int random = ran.nextInt(99) + 1;
+		prob = prob * 100;
+		System.out.println("PROB: " + prob + " RANDOM: " + random);
+		if (prob > random) {
+			nLost++;
+			totalLost++;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Slow Start phase i is acting as cwnd should double after each RTT, so time
+	 * should prob go here
+	 */
+	public static void sStart() {
+		if (slowStart == true) {
+			int i = 1;
+			while (i < ssthresh && slowStart == true && nLost < 3 && pubData > pubMSS) {
+				System.out.println("I" + i);
+				int itemp = i * 2;
+				for (int j = i; j < itemp; j++) {
+					if (pubData - pubMSS > 0) {
+						toServer(cPackets.get(cPackets.size() - 1), pubProb);
+						toClient(pubMSS);
+						pubData = pubData - pubMSS;
+						pubTime += pubTime;
+						// send second to last packet
+					} else {
+						toServer(cPackets.get(cPackets.size() - 1), pubProb);
+						toClient(pubData);
+						pubTime += pubTime;
+						break;
+					}
+				}
+				i = itemp;
+			}
+			cwnd = i;
+		}
+		if (cwnd > ssthresh || nLost >= 3) {
+			ssthresh = cwnd / 2;
+			slowStart = false;
+			cwnd = 1;
+		}
+	}
+
+	public static void congestionAvoidance() {
+		ssthresh = cwnd / 2;
+		slowStart = false;
+		cwnd = 1;
+		while (cwnd < ssthresh) {
+			for(int i = 0; i < cwnd; i++) {
+				
+			}
+			cwnd = (cwnd + 1) / cwnd;
+		}
 	}
 
 }
